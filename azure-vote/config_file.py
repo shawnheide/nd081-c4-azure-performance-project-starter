fb9013b881c6f5c2260b978fad13b8154c5b552b
Shawn Heide <shawnheide@gmail.com>
2021-12-09 22:03:37 -0800

    feat: initial code update for azure-vote

--- azure-vote/config_file.py
+++ azure-vote/config_file.py
@@ -1,5 +1,8 @@
+import os
+
 # UI Configurations
 TITLE = 'Azure Voting App'
 VOTE1VALUE = 'Cats'
 VOTE2VALUE = 'Dogs'
 SHOWHOST = 'false'
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
--- azure-vote/main.py
+++ azure-vote/main.py
@@ -1,46 +1,71 @@
 from flask import Flask, request, render_template
 import os
-import random
 import redis
 import socket
-import sys
 import logging
-from datetime import datetime
 
 # App Insights
-# TODO: Import required libraries for App Insights
-
-# Logging
-logger = # TODO: Setup logger
-
-# Metrics
-exporter = # TODO: Setup exporter
-
-# Tracing
-tracer = # TODO: Setup tracer
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import stats as stats_module
+from opencensus.trace import config_integration
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 app = Flask(__name__)
 
-# Requests
-middleware = # TODO: Setup flask middleware
-
 # Load configurations from environment or config file
-app.config.from_pyfile('config_file.cfg')
+app.config.from_pyfile('config_file.py')
 
-if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
-    button1 = os.environ['VOTE1VALUE']
-else:
-    button1 = app.config['VOTE1VALUE']
+connection_string = app.config.get("APP_INSIGHTS_CONNECTION_STRING")
 
-if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
-    button2 = os.environ['VOTE2VALUE']
-else:
-    button2 = app.config['VOTE2VALUE']
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests'])
 
-if ("TITLE" in os.environ and os.environ['TITLE']):
-    title = os.environ['TITLE']
-else:
-    title = app.config['TITLE']
+# Setup logger
+logger = logging.getLogger(__name__)
+
+# Logging python logs
+log_handler = AzureLogHandler(connection_string=connection_string)
+log_handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+logger.addHandler(log_handler)
+
+# Logging custom Events
+event_handler = AzureEventHandler(connection_string=connection_string)
+logger.addHandler(event_handler)
+
+# Set the logging level
+logger.setLevel(logging.INFO)
+
+# Metrics
+stats = stats_module.stats
+view_manager = stats.view_manager
+
+exporter = metrics_exporter.new_metrics_exporter(enable_standard_metrics=True, connection_string=connection_string)
+view_manager.register_exporter(exporter)
+
+# Tracing
+azure_exporter = AzureExporter(connection_string=connection_string)
+sampler = ProbabilitySampler(1.0)
+tracer = Tracer(exporter=azure_exporter, sampler=sampler)
+
+# Requests
+middleware = FlaskMiddleware(app, exporter=azure_exporter, sampler=sampler)
+
+def get_config_value(name):
+    if (name in os.environ and os.environ[name]):
+        value = os.environ[name]
+    else:
+        value = app.config[name]
+
+    return value
+
+button1 = get_config_value('VOTE1VALUE')
+button2 = get_config_value('VOTE2VALUE')
+title = get_config_value('TITLE')
 
 # Redis Connection
 r = redis.Redis()
@@ -53,19 +78,28 @@ if app.config['SHOWHOST'] == "true":
 if not r.get(button1): r.set(button1,0)
 if not r.get(button2): r.set(button2,0)
 
+def get_vote_count(button):
+    return int(r.get(button).decode('utf-8'))
+
+def trace_vote(vote_name):
+    with tracer.span(name=vote_name) as span:
+        print(vote_name)
+
+def log_votes(vote_name, n_votes):
+    properties = {'custom_dimensions': {vote_name: n_votes}}
+    logger.info(vote_name, extra=properties)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'GET':
 
         # Get current values
-        vote1 = r.get(button1).decode('utf-8')
-        # TODO: use tracer object to trace cat vote
-        vote2 = r.get(button2).decode('utf-8')
-        # TODO: use tracer object to trace dog vote
-
-        # Return index with values
-        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+        n_votes_cat = get_vote_count(button1)
+        trace_vote('Cats Vote')
+        
+        n_votes_dog = get_vote_count(button2)
+        trace_vote('Dogs Vote')
 
     elif request.method == 'POST':
 
@@ -74,15 +108,6 @@ def index():
             # Empty table and return results
             r.set(button1,0)
             r.set(button2,0)
-            vote1 = r.get(button1).decode('utf-8')
-            properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            # TODO: use logger object to log cat vote
-
-            vote2 = r.get(button2).decode('utf-8')
-            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            # TODO: use logger object to log dog vote
-
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
         else:
 
@@ -90,15 +115,19 @@ def index():
             vote = request.form['vote']
             r.incr(vote,1)
 
-            # Get current values
-            vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')
+        # Get current values
+        n_votes_cat = get_vote_count(button1)
+        log_votes('Cats Vote', n_votes_cat)
 
-            # Return results
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+        n_votes_dog = get_vote_count(button2)
+        log_votes('Dogs Vote', n_votes_dog)
+
+    # Return results
+    return render_template("index.html", value1=n_votes_cat, value2=n_votes_dog, button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
     # comment line below when deploying to VMSS
-    app.run() # local
+    # app.run() # local
+
     # uncomment the line below before deployment to VMSS
-    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
